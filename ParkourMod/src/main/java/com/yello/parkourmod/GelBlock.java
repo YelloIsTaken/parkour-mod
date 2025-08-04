0621eca37939fa611a055599699228cbf8ea8d2a
Yello <bartus.nowaczyk@gmail.com>
2025-08-04 13:44:53 +0200

    Add textures, properties and loot tables to Gel Block and Rubber Block

--- ParkourMod/src/main/java/com/yello/parkourmod/GelBlock.java
+++ ParkourMod/src/main/java/com/yello/parkourmod/GelBlock.java
@@ -4,14 +4,18 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class GelBlock extends Block {
     public GelBlock(Properties properties){
-        super(properties);
+        super(properties.strength(0.5f).sound(SoundType.HONEY_BLOCK).noOcclusion());
     }
     @Override
     public void fallOn(Level level, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
         // Do nothing: cancels fall damage
+    }
+    public boolean isToolRequired(BlockState state) {
+        return false; // No tool required, can break by hand
     }
 }
--- ParkourMod/src/main/java/com/yello/parkourmod/RubberBlock.java
+++ ParkourMod/src/main/java/com/yello/parkourmod/RubberBlock.java
@@ -6,10 +6,11 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.SoundType;
 
 public class RubberBlock extends Block {
     public RubberBlock(Properties properties){
-        super(properties);
+        super(properties.strength(0.5f).sound(SoundType.HONEY_BLOCK).noOcclusion());
     }
 
     @Override
@@ -20,5 +21,8 @@ public class RubberBlock extends Block {
     @Override
     public void fallOn(Level level, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
         // Do nothing: cancels fall damage
+    }
+    public boolean isToolRequired(BlockState state) {
+        return false; // No tool required, can break by hand
     }
 }
